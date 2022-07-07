00254ba29f0415ac89bbe0e3a83d26539965bc1b
Isaac Reyes <isaacjdjreyes@gmail.com>
2022-07-07 18:34:56 +0800

    Add decimal functionality

--- js-file.js
+++ js-file.js
@@ -41,13 +41,22 @@ const numbers = document.querySelectorAll(".num");
 const equal = document.querySelector("#equals");
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
+const decimal = document.querySelector("#decimal");
 let numA = "";
 let numB = "";
 let currentOperator = "";
 
+// Extra features
+// - Decimal: Done.
+// - Backspace
+//      - Change operation functionality
+// - Exponents
+// - Keyboard Compatibility
+// - Negation
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        if(button.textContent !== "=") {
+        if(!button.classList.contains("func")) {
             display.textContent += button.textContent;
         }
         if(button.textContent == "C") {
@@ -61,36 +70,47 @@ buttons.forEach((button) => {
 numbers.forEach((button) => {
     button.addEventListener("click", () => {
         if(currentOperator == "") {
-            numA += button.textContent;
-            console.log(numA);
+            if(!numA.includes(".")) {
+                numA += button.textContent;
+            } else if(numA.includes(".")) {
+                numA += button.textContent;
+                decimal.disabled = true;
+            }
+            console.log("numA: " + numA);
         } else if(currentOperator != "") {
-            numB += button.textContent;
-            console.log(numB);
-
+            decimal.disabled = false;
+            if(!numB.includes(".")) {
+                numB += button.textContent;
+            } else if(numB.includes(".")) {
+                numB += button.textContent;
+                decimal.disabled = true;
+            }
+            console.log("numB: " + numB);
         }
     });
 });
-
 operators.forEach((button) => {
     button.addEventListener("click", () => {
         if(numA !== "" && numB !== "" && currentOperator !== "") {
             numA = operate(isOperator(currentOperator), +numA, +numB);
             numB = "";
+            decimal.disabled = false;
             display.textContent = "=" + numA + button.textContent;
         }
         currentOperator = button.textContent;
-        console.log(currentOperator);
+        console.log("operator: " + currentOperator);
     });
 });
-
 equal.addEventListener("click", () => {
     if(numA !== "" && numB !== "" && currentOperator !== "") {
         numA = operate(isOperator(currentOperator), +numA, +numB);
         numB = "";
+        decimal.disabled = false;
         currentOperator = "";
         display.textContent = "=" + numA;
     }
 });
+
 
 function isOperator(char) {
     if(char == "+") {
